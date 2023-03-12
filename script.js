d5e534f9ba183001e2e3b7ac03dc432255583aac
Warren Coffey <warrencoffey@googlemail.com>
2023-03-12 21:54:29 +0000

    Added UI

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let playerWins = 0;
 let computerWins = 0;
+let round = 0;
+
+const container = document.querySelector("#container");
+const results = document.createElement("div");
 
 function getComputerChoice() {
     let randomNumber = getRandomNumber();
@@ -17,47 +21,124 @@ function getComputerChoice() {
 }
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    return Math.floor(Math.random() * 3) + 1;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
+    round++;
+    const playerSelection = e.target.value;
+    const computerSelection = getComputerChoice();
+    const resultText = document.createElement("p");
+    let result;
+
     if (playerSelection === computerSelection) {
-        return "draw";
+        result = "Draw";
     }
 
     else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")) {
         playerWins++;
-        return "win";
+        result = "Win";
     }
 
     else {
         computerWins++;
-        return "lose"
+        result = "Lose";
+    }
+
+    resultText.textContent = `Round ${round} | ${result}! | ${playerWins}-${computerWins} | ${playerSelection} vs. ${computerSelection}`;
+    results.appendChild(resultText);
+
+    const winner = checkWinner();
+
+    if (winner) {
+        const win = document.createElement("p");
+        win.style.fontWeight = "600";
+        win.textContent = `THE WINNER IS... ${winner.toUpperCase()}`;
+        results.appendChild(win);
+        disableButtons();
     }
 }
 
+function disableButtons() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => button.setAttribute("disabled", true));
+}
 
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Rock? Paper? Scissors?").toLowerCase();
-        console.log(`Round ${i+1}: ` + playRound(playerSelection, computerSelection) + ` (${playerSelection} vs. ${computerSelection})`);
+function checkWinner() {
+    if (playerWins === 5) {
+        return "player";
     }
 
-    if (playerWins > computerWins) {
-        return "Winner!"
-    }
-
-    else if (playerWins === computerWins) {
-        return "Draw!"
+    else if (computerWins === 5) {
+        return "computer";
     }
 
     else {
-        return "Loser!"
+        return null
     }
 }
 
-console.log(game() + ` | Final score: ${playerWins}-${computerWins}`);
+function createButtons() {
+    const rockBtn = document.createElement("button");
+    rockBtn.textContent = "Rock";
+    rockBtn.value = "rock";
+
+    const paperBtn = document.createElement("button");
+    paperBtn.textContent = "Paper";
+    paperBtn.value = "paper";
+
+    const scissorsBtn = document.createElement("button");
+    scissorsBtn.textContent = "Scissors";
+    scissorsBtn.value = "scissors";
+
+    container.appendChild(rockBtn);
+    container.appendChild(paperBtn);
+    container.appendChild(scissorsBtn);
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => button.addEventListener("click", playRound));
+}
+
+function setUpGame() {
+    createButtons();
+    createResultsDiv();
+}
+
+function createResultsDiv() {
+    container.appendChild(results);
+
+    const header = document.createElement("h4");
+    header.textContent = "Results";
+    results.appendChild(header);
+
+}
+
+
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         const computerSelection = getComputerChoice();
+//         const playerSelection = prompt("Rock? Paper? Scissors?").toLowerCase();
+//         console.log(`Round ${i + 1}: ` + playRound(playerSelection, computerSelection) + ` (${playerSelection} vs. ${computerSelection})`);
+//     }
+
+//     if (playerWins > computerWins) {
+//         return "Winner!"
+//     }
+
+//     else if (playerWins === computerWins) {
+//         return "Draw!"
+//     }
+
+//     else {
+//         return "Loser!"
+//     }
+// }
+
+// console.log(game() + ` | Final score: ${playerWins}-${computerWins}`);
+
+
+setUpGame();
